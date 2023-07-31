d41f512067a35656e6b0664a31a0e92520036f7c
Long <Truonghoanglong1802@gmail.com>
2023-07-31 19:25:05 +0700

    First Post

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: "cluster0.gj6ptvq.mongodb.net",
+  DB_USER: "lolpro342",
+  DB_PASS: "truonglong145",
+  DB_DATABASE: "webbanhangonline",
+  EMAIL_USER: "TruongHoangLong1802@gmail.com", // Microsoft mail service
+  EMAIL_PASS: "masxiyelndcruhhz",
+  JWT_SECRET: "123",
+  JWT_EXPIRES: "1000000000", // in milliseconds
+};
+module.exports = MyConstants;
